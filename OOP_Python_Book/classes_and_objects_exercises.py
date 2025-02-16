efec745dc91071fa2a672ec75b0e6ad3f5fb848f
vd141 <victor.dinh141@gmail.com>
2025-02-15 16:42:29 -0800

    second pass

--- OOP_Python_Book/classes_and_objects_exercises.py
+++ OOP_Python_Book/classes_and_objects_exercises.py
@@ -1,121 +1,130 @@
-class Car:
-    def __init__(self, model, year, color):
-        self.__model = model
-        self.__year = year
-        self.__color = color
+# class Car:
+#     def __init__(self, model, year, color):
+#         self._model = model
+#         self._year = year
+#         self._color = color
+#         self._speed = 0
 
-        self.__speed = 0
+#     @classmethod
+#     def mileage(cls, miles, gallons):
+#         mpg = miles / gallons
+#         print(f'Miles per gallon is {mpg:.2f}.')
 
-    @classmethod
-    def mpg(cls, miles, gallons):
-        return miles / gallons
-
-    def engine_on(self):
-        print(f'The {self.__color} {self.__year} {self.__model} is now on.')
-
-    def accelerate(self, speed):
-        print(f'The {self.__color} {self.__year} {self.__model} is now accelerating '
-              f'by {speed} mph.')
-        self.__speed += speed
-
-    def brake(self, speed):
-        print(f'The {self.__color} {self.__year} {self.__model} is now braking by '
-              f'{speed} mph.')
-        self.__speed -= speed
-
-    def engine_off(self):
-        self.speed = 0
-        print(f'The {self.__color} {self.__year} {self.__model} is now off.')
-
-    def current_speed(self):
-        print(f'The {self.__color} {self.__year} {self.__model}\'s current speed is '
-              f'{self.__speed}')
-        
-    @property
-    def color(self):
-        return self.__color
+#     @property
+#     def speed(self):
+#         return self._speed
     
-    @color.setter
-    def color(self, color):
-        self.__color = color
-
-    @property
-    def model(self):
-        return self.__model
+#     @speed.setter
+#     def speed(self, new_speed):
+#         self._speed = new_speed
+#         return self._speed
     
-    @property
-    def year(self):
-        return self.__year
+#     @property
+#     def color(self):
+#         return self._color
     
-    def spray_color(self, color):
-        print(f'The car is being sprayed {color}!')
-        self.__color = color
+#     @color.setter
+#     def color(self, new_color):
+#         self._color = new_color
 
-    @staticmethod
-    def driver_message(sentence):
-        print(f'VROOM VROOM {sentence}')
+#     @property
+#     def model(self):
+#         return self._model
+    
+#     @property
+#     def year(self):
+#         return self._year
 
-# toyota = Car('Crosstrek', 2025, 'Gray')
-# toyota.engine_on()
-# toyota.accelerate(35)
-# toyota.current_speed()
-# toyota.brake(21)
-# toyota.current_speed()
-# toyota.brake(14)
-# toyota.current_speed()
-# toyota.engine_off()
-# toyota.current_speed()
+#     def engine_on(self):
+#         if self._speed == 0:
+#             print('Engine is on! Speed is 0.')
+#         else:
+#             print(f'Engine is already running and speed is {self._speed}')
+#         pass
+
+#     def accelerate(self, new_speed):
+#         self._speed = new_speed
+#         print(f'Accelerating. Speed increased to {new_speed}')
+
+#     def brake(self, new_speed):
+#         self._speed = new_speed
+#         print(f'Braking. Speed reduced to {new_speed}')
+
+#     def engine_off(self):
+#         if self.speed == 0:
+#             print('Engine is now off. Speed is 0.')
+#         else:
+#             print('Brake to a stop before turning engine off!')
+#         self._speed = 0
+
+#     def spray_paint(self, new_color):
+#         self._spray_paint_color = new_color
+#         self._color = self._spray_paint_color
+
+# toyota = Car('prius', '2025', 'white')
+
+# # toyota.engine_on()
+# # toyota.accelerate(40)
+# # toyota.brake(20)
+# # toyota.brake(0)
+# # toyota.engine_off()
+
 # print(toyota.color)
-# toyota.color = 'astrid'
+# toyota.color = 'pink'
 # print(toyota.color)
 # print(toyota.model)
 # print(toyota.year)
-# toyota.spray_color('cerulean')
-# print(Car.mpg(47, 1.2))
+# toyota.spray_paint('yellow')
+# print(toyota.color)
+# # toyota.model = 'camry'
+# # toyota.year = 2024
 
+# Car.mileage(47, 2.5)
 
 class Person:
     def __init__(self, first, last):
-        self.__name_setter((first, last))
+        if self._validate(first):
+            self._first = first
+        if self._validate(last):
+            self._last = last
 
     @property
-    def full_name(self):
-        return self.__first.title() + ' ' + self.__last.title()
+    def name(self):
+        return ' '.join([self._first.title(), self._last.title()])
     
-    @full_name.setter
-    def full_name(self, full_name_tuple):
-        self.__name_setter(full_name_tuple)
-
-    @classmethod
-    def __validate(cls, full_name_tuple):
-        if '' in full_name_tuple:
-            raise ValueError('All characters must be alphabetic.')
-        if not isinstance(full_name_tuple, tuple):
+    @name.setter
+    def name(self, new_name):
+        if not isinstance(new_name, tuple):
             raise TypeError('Input must be a tuple.')
-        for char in set(full_name_tuple[0] + full_name_tuple[1]):
+            return False
+        if self._validate(new_name[0]) and self._validate(new_name[1]):
+            self._first, self._last = new_name
+    
+    @staticmethod
+    def _validate(name):
+        if name == '':
+            raise ValueError('All characters must be alphabetic')
+            return False
+        for char in name:
             if not char.isalpha():
-                raise ValueError('All characters must be alphabetic.')
-
-    def __name_setter(self, full_name_tuple):
-        Person.__validate(full_name_tuple)
-        self.__first, self.__last = full_name_tuple
-
-
+                raise ValueError('All characters must be alphabetic')
+                return False
+        
+        return True
+    
 # actor = Person('Mark', 'Sinclair')
-# print(actor.full_name)              # Mark Sinclair
-# actor.full_name = ('Vin', 'Diesel')
-# print(actor.full_name)              # Vin Diesel
-# actor.full_name = ('', 'Diesel')
+# print(actor.name)              # Mark Sinclair
+# actor.name = ('Vin', 'Diesel')
+# print(actor.name)              # Vin Diesel
+# actor.name = ('', 'Diesel')
 # # ValueError: Name must be alphabetic.
 
 # character = Person('annIE', 'HAll')
-# print(character.full_name)          # Annie Hall
+# print(character.name)          # Annie Hall
 # character = Person('Da5id', 'Meier')
 # # ValueError: Name must be alphabetic.
 
-# friend = Person('Lynn', 'Blake')
-# print(friend.full_name)             # Lynn Blake
-# friend.full_name = ('Lynn', 'Blake-John')
-# # ValueError: Name must be alphabetic.
-
-subaru = Car.driver_message('hihi hehe')
+friend = Person('Lynn', 'Blake')
+print(friend.name)             # Lynn Blake
+friend.name = ('Lynn', 'Blake-John')
+# ValueError: Name must be alphabetic.
