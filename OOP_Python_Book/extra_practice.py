5a2da6e53131cbff08b890fbdad21f21570c44b3
vd141 <victor.dinh141@gmail.com>
2025-03-10 16:42:41 +0700

    updated with notes from first pass of OOP exercise set 1

--- OOP_Python_Book/extra_practice.py
+++ OOP_Python_Book/extra_practice.py
@@ -2,3 +2,11 @@
 # problem 6 from above
 # problem 3 (challenge) from https://launchschool.com/books/oo_python/read/magic_methods#exercises
 # 
+
+# single vs double underscore in front of instance variables
+    # single underscore is the most common way to indicate that an instance variable
+    # is intended for interal use
+
+    # double underscore (also called name mangling) prepends _ClassName to the 
+    # instance variable, preventing it from being overrided by a subclass
+    # attribute
