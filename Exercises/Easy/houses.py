fb6bd8f72af45586b92144113e06244d3b316965
Victor Dinh <vd141@users.noreply.github.com>
2025-03-25 20:23:14 +0000

    first pass

---
+++ Exercises/Easy/houses.py
@@ -0,0 +1,25 @@
+class House:
+    def __init__(self, price):
+        self._price = price
+
+    def __lt__(self, other):
+        return self.price < other.price
+    
+    def __gt__(self, other):
+        return self.price > other.price
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        self._price = value
+
+home1 = House(100000)
+home2 = House(150000)
+if home1 < home2:
+    print("Home 1 is cheaper")
+if home2 > home1:
+    print("Home 2 is more expensive")
+
