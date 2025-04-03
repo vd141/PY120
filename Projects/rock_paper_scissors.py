6125916b7f1830ce8110d4dd191e3705d6b29cd3
Victor Dinh <vd141@users.noreply.github.com>
2025-04-03 20:37:29 +0000

    first implementation fo game

--- Projects/rock_paper_scissors.py
+++ Projects/rock_paper_scissors.py
@@ -1,3 +1,81 @@
 '''
 OOP RPS
 '''
+import random
+
+class Player:
+    CHOICES = ('rock', 'paper', 'scissors')
+
+    def __init__(self, player_type):
+        # maybe a "name"? what about a "move"?
+        self._player_type = player_type.lower()
+        self.player_choice = None
+
+    def _is_human(self):
+        return self._player_type == 'human'
+
+    def choose(self):
+        if self._is_human():
+            while True:
+                prompt = 'Select (rock), (paper), or (scissors): '
+                choice = input(prompt)
+                if choice in Player.CHOICES:
+                    break
+                print('Invalid choice. ', end='')
+            self.player_choice = choice
+        else:
+            self.player_choice = random.choice(Player.CHOICES)
+
+class RPSGame:
+    def __init__(self):
+        self._human = Player('human')
+        self._computer = Player('computer')
+
+    def _display_welcome_message(self):
+        print('Welcome to Rock Paper Scissors!')
+
+    def _display_goodbye_message(self):
+        print('Thanks for playing Rock Paper Scissors. Goodbye!')
+
+    def _display_winner(self):
+        '''
+        compare player choice to computer choice
+        '''
+        human_move = self._human.player_choice
+        computer_move = self._computer.player_choice
+        print(f'You chose {human_move}.')
+        print(f'The computer chose {computer_move}.')
+
+        # player wins
+        if ((human_move == 'rock' and computer_move == 'scissors') or
+            (human_move == 'paper' and computer_move == 'rock') or
+            (human_move == 'scissors' and computer_move == 'paper')):
+            
+            print('You win!')
+        # computer wins
+        elif ((computer_move == 'rock' and human_move == 'scissors') or
+            (computer_move == 'paper' and human_move == 'rock') or
+            (computer_move == 'scissors' and human_move == 'paper')):
+
+            print('Computer wins!')
+        # tie
+        else:
+            print('It\'s a tie!')
+
+    def _play_again(self):
+        prompt = 'Would you like to play again? y/n: '
+        return input(prompt).lower().startswith('y')
+         
+
+    def play(self):
+        self._display_welcome_message()
+
+        while True:
+            self._human.choose()
+            self._computer.choose()
+            self._display_winner()
+            if not self._play_again():
+                break
+        self._display_goodbye_message()
+
+RPSGame().play()
