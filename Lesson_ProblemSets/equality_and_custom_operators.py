eb45ec1b08ab44a4df930ddf7505b1282f2e8fc8
Victor Dinh <vd141@users.noreply.github.com>
2025-04-08 14:28:46 +0000

    completed first pass

--- Lesson_ProblemSets/equality_and_custom_operators.py
+++ Lesson_ProblemSets/equality_and_custom_operators.py
@@ -114,24 +114,63 @@ class Vector:
         return Vector(self.x * other, self.y * other)
     
     def __rmul__(self, other):
-        if not isinstance(other, Vector):
+        if not isinstance(other, int):
             return NotImplemented
         
-        return Vector(self * other.x, self * other.y)
+        return Vector(self.x * other, self.y * other)
 
 
-print(Vector(3, 2) + Vector(5, 12))   # Vector(8, 14)
-print(Vector(5, 12) - Vector(3, 2))   # Vector(2, 10)
-print(Vector(5, 12) * 2)              # Vector(10, 24)
-print(3 * Vector(5, 12))              # Vector(15, 36)
+# print(Vector(3, 2) + Vector(5, 12))   # Vector(8, 14)
+# print(Vector(5, 12) - Vector(3, 2))   # Vector(2, 10)
+# print(Vector(5, 12) * 2)              # Vector(10, 24)
+# print(3 * Vector(5, 12))              # Vector(15, 36)
 
-my_vector = Vector(5, 7)
-my_vector += Vector(3, 9)
-print(my_vector)                      # Vector(8, 16)
+# my_vector = Vector(5, 7)
+# my_vector += Vector(3, 9)
+# print(my_vector)                      # Vector(8, 16)
 
-my_vector -= Vector(1, 7)
-print(my_vector)                      # Vector(7, 9)
+# my_vector -= Vector(1, 7)
+# print(my_vector)                      # Vector(7, 9)
 
-print(Vector(3, 2) + 5)
-# TypeError: unsupported operand type(s) for +: 'Vector'
-# and 'int'
+# print(Vector(3, 2) + 5)
+# # TypeError: unsupported operand type(s) for +: 'Vector'
+# # and 'int'
+
+class Silly:
+    def __init__(self, value):
+        if isinstance(value, int):
+            self.value = value
+        else:
+            self.value = str(value)
+
+    def __str__(self):
+        return f'Silly({repr(self.value)})'
+
+    def __add__(self, other):
+        '''
+        assuming Silly(x) + y
+
+        if either x or y is a non-numeric string, concatenate the string values of x and y
+        otherwise, compute the sum of the integer values of x and y
+        '''
+
+        if not isinstance(other, int):
+            if not isinstance(other, str):
+                return NotImplemented
+            
+        try:
+            other_int = int(other)
+            self_int = int(self.value)
+
+            return Silly(other_int + self_int)
+        except:
+            return Silly(str(other) + str(self.value))
+
+
+print(Silly('abc') + 'def')        # Silly('abcdef')
+print(Silly('abc') + 123)          # Silly('abc123')
+print(Silly(123) + 'xyz')          # Silly('123xyz')
+print(Silly('333') + 123)          # Silly(456)
+print(Silly(123) + '222')          # Silly(345)
+print(Silly(123) + 456)            # Silly(579)
+print(Silly('123') + '456')        # Silly(579)
