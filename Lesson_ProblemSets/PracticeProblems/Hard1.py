1988a20a2119d4d432eda42cd9663ba864c9e2f2
vd141 <victor.dinh141@gmail.com>
2025-04-12 12:19:03 -0400

    first pass

--- Lesson_ProblemSets/PracticeProblems/Hard1.py
+++ Lesson_ProblemSets/PracticeProblems/Hard1.py
@@ -1,9 +1,11 @@
 '''
 1
-
-
 '''
-class WheeledVehicle:
+class VehicleMixIn:
+    def range(self):
+        return self.fuel_capacity * self.fuel_efficiency
+
+class WheeledVehicle(VehicleMixIn):
     def __init__(self,
                  tire_list,
                  kilometers_per_liter,
@@ -18,9 +20,6 @@ class WheeledVehicle:
     def inflate_tire(self, tire_index, pressure):
         self.tires[tire_index] = pressure
 
-    def range(self):
-        return self.fuel_capacity * self.fuel_efficiency
-
 class Auto(WheeledVehicle):
     def __init__(self):
         # 4 tires with various tire pressures
@@ -30,3 +29,33 @@ class Motorcycle(WheeledVehicle):
     def __init__(self):
         # 2 tires with various tire pressures
         super().__init__([20, 20], 80, 8.0)
+
+class Watercraft(VehicleMixIn):
+    def __init__(self,
+                number_propellers,
+                number_hulls,
+                kilometers_per_liter,
+                liters_of_fuel_capacity):
+        
+        self.number_propellers = number_propellers
+        self.number_hulls = number_hulls
+        self.fuel_efficiency = kilometers_per_liter
+        self.fuel_capacity = liters_of_fuel_capacity
+
+    def range(self):
+        return super().range()+ 10
+
+class Catamaran(Watercraft):
+    pass
+
+'''
+2
+'''
+class Motorboat(Watercraft):
+    def __init__(self, kilometers_per_liter, liters_of_fuel_capacity):
+        super().__init__(self, 1, 1,
+                         kilometers_per_liter, liters_of_fuel_capacity)
+        
+'''
+3
+'''
