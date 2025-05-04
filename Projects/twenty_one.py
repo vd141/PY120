ae07ecd03a984b719bedb0ac27f83a7e50281af5
vd141 <victor.dinh141@gmail.com>
2025-05-04 14:22:20 -0400

    init commit

---
+++ Projects/twenty_one.py
@@ -0,0 +1,9 @@
+'''
+1. write a textual description of the problem or exercise
+
+2. extract the significant nouns and verbs from the description
+
+3. organize and associate the verbs with the nouns
+
+4. write scaffolding and spike code
+'''
