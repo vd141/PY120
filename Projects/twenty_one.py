694daa86e6d46b055debb67f913fc6b7aa1b235e
vd141 <victor.dinh141@gmail.com>
2025-05-04 16:54:43 -0400

    midday progress

--- Projects/twenty_one.py
+++ Projects/twenty_one.py
@@ -1,7 +1,45 @@
 '''
 1. write a textual description of the problem or exercise
+    - card game with a dealer and a player
+    - deck has 52 cards
+    - participants try to get as close to 21 as possible without going over
+    - both participants receive two cards
+        - dealer hides one of their cards
+        - player can see both of their cards
+    
+    - player takes the first turn and can either hit or stay
+    - if the player's hand surpasses 21, the player busts and the dealer wins
+    - if the player stays, the dealer plays next
+
+    - if the player didn't bust and chose to stay, it is the dealer's turn
+    - dealer reveals face-down card
+    - if dealer's hand is under 17, dealer automatically hits
+    - dealer busts if hand goes over 21
+    - if dealer has 17 points or more, dealer stays
+
+    - results of game are determined
 
 2. extract the significant nouns and verbs from the description
+    - nouns 
+        - deck
+            - 52 cards
+        - card
+            - value
+            - suit
+        - dealer
+            - hand
+            - points
+            - strategy
+        - player
+            - hand
+            - points
+            - strategy
+        - twenty-one game
+            - rules
+                - bust
+                - 21
+            - flow
+        
 
 3. organize and associate the verbs with the nouns
 
